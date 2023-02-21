f568e1cb9212870d0ec2edc4519055f2135cf0f3
Alan Khorsid <alan18@hotmail.dk>
2023-02-21 08:43:41 +0100

    BadaBing

--- src/main.py
+++ src/main.py
@@ -2,9 +2,8 @@ import requests
 
 url = "https://www.wikidata.org/w/api.php"
 
-
 while True:
-    query = input("Enter name : ")
+    query = input("Enter name: ")
     if query == "quit":
         break
     else:
@@ -12,12 +11,27 @@ while True:
             "action": "wbsearchentities",
             "language": "en",
             "format": "json",
-            "search": query
+            "search": query,
+            "limit": "10",
         }
         try:
+            id_list = []
             data = requests.get(url, params=params)
-            print(data.json()["search"][2]["description"])
-            print(data.json()["search"][2]["id"])
+            for i in range(len(data.json()["search"])):
+                print(data.json()["search"][i]["label"])
+                print(data.json()["search"][i]["id"])
+                id_list.append(data.json()["search"][i]["id"])
 
+            for j in range(len(id_list)):
+                params = {
+                    "action": "wbgetentities",
+                    "languages": "en",
+                    "format": "json",
+                    "ids": id_list[j],
+                    "props": "claims",
+                }
+                data = requests.get(url, params=params)
+                print(id_list[j])
+                print(data.json())
         except:
             print("Invalid Input try again !!!")
