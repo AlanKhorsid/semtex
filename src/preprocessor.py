b7508b0fa81a9a01fdf7cce7db3a619f032c5c21
asa19aau <asa19@student.aau.dk>
2023-02-27 12:46:56 +0000

    added a function that provides symbol cleaning

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -2,6 +2,7 @@
 #import numpy
 from autocorrect import Speller
 from spellchecker import SpellChecker
+from itertools import chain, combinations
 
 def spellcheck(inputString: str) -> {str}:
     spell = SpellChecker()
@@ -18,10 +19,32 @@ def autocorrect(inputString: str) -> str:
     autocorrectedString = spell(inputString.lower())
 
     return autocorrectedString
+#def powerset(iterable):
+#    s = list(iterable)
+#    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
-string = "salin"
+def removeUnwantedCharacters(inputString: str, symbolsToRemove = ["!", "\n", "."]) -> [str]:
+    candidateStrings = []
+    pSet =  chain.from_iterable(combinations(list(symbolsToRemove), r) for r in range(len(symbolsToRemove)+1))
 
-x = spellcheck(string)
+    for j in pSet:
+        cleanedString = inputString
+        for k in j:
+            cleanedString = cleanedString.replace(k, " ")
+
+        if cleanedString not in candidateStrings:
+            candidateStrings.append(cleanedString)
+
+    for i in range(len(candidateStrings)):
+        candidateStrings[i] = candidateStrings[i].strip()
+
+    return candidateStrings
+
+strings = removeUnwantedCharacters("\"trtle.prsident!obma\"")
+
+print(f"strings: {strings}\n")
+x = []
+for string in strings:
+    x.append(spellcheck(string))
 for cand in x:
     print(cand)
-print(autocorrect(string))
