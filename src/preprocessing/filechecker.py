025dcad87cee3c1406eeb536c5da5c58308d3215
asa19aau <asa19@student.aau.dk>
2023-02-28 13:50:01 +0000

    added comments

--- src/preprocessing/filechecker.py
+++ src/preprocessing/filechecker.py
@@ -2,21 +2,24 @@ from pathlib import Path
 import os
 
 def check_if_file_exists(filename: str) -> bool:
+    """Takes an input filename and checks if it is in the datasets/webpages-json folder, returns boolean True or False."""
     filepath = str(Path(__file__).parent.parent.parent) + "/datasets/webpages-json/" + str(filename)
     return Path(filepath).is_file()
 
-def read_existing_file(filename: str) -> str:
-    pass
-
 def check_missing_files() -> [str]:
+    """Checks for all files in src/preprocessing/tables and checks if a file exists for each entity value, returns a list of missing entity value json files."""
+
     text_files_dir = str(Path(__file__).parent.parent.parent) +  "/src/preprocessing/tables"
     missing_files = []
+
+    #Generates the filenames for each entity value json file to check if it exists
     for filename in os.listdir(text_files_dir):
         with open(os.path.join(text_files_dir, filename), "r") as f:
             stripped_filename = filename.strip(".csv")
             for i, line in enumerate(f):
                 if i == 0:
                     continue
+                #generates the name of a .json file to lookup
                 json_filename = stripped_filename + "_" + str(i) + ".json"
                 fields = line.strip().split(",")
 
@@ -25,7 +28,8 @@ def check_missing_files() -> [str]:
                         missing_files.append(json_filename)
                         #call suggester from here
                         print(json_filename)
-
+    
+    #returns the list of missing json files
     return missing_files
 
 
