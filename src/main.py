bed74fecc9fbd8efa85d07a78e3377ce5f2ffb52
esb179 <esbenn179@gmail.com>
2023-03-31 14:04:14 +0200

    here

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from catboost import CatBoostClassifier, CatBoostRegressor, Pool
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import ParameterGrid, train_test_split
 from classes import Column
 from util import (
     ensemble_catboost_regression,
@@ -61,13 +61,18 @@ with progress:
 
 # ----- Train model -----
 features = []
-for col in progress.track(cols_validation, description="Training model"):
+features_test = []
+for col in progress.track(cols, description="Training model"):
     features.extend(col.features)
+
+for col in progress.track(cols_validation, description="Training model"):
+    features_test.extend(col.features)
+
 # max_id = max([i[0] for i in features])
 # features = [[x[0] / max_id] + x[1:] for x in features]
 
 # Process features
-X = pd.DataFrame(
+test = pd.DataFrame(
     {
         "id": [x[0] for x in features],
         "title": [x[1] for x in features],
@@ -80,11 +85,23 @@ X = pd.DataFrame(
         "label": [x[8] for x in features],
     }
 )
+train = pd.DataFrame(
+    {
+        "id": [x[0] for x in features_test],
+        "title": [x[1] for x in features_test],
+        "description": [x[2] for x in features_test],
+        "num_statements": [x[3] for x in features_test],
+        "instance_overlap": [x[4] for x in features_test],
+        "subclass_overlap": [x[5] for x in features_test],
+        "description_overlap": [x[6] for x in features_test],
+        "instance_names": [x[7] for x in features_test],
+        "label": [x[8] for x in features_test],
+    }
+)
 
 text_features = ["title", "description", "instance_names"]
-# text_features = []
 
-train, test = train_test_split(X, test_size=0.3, random_state=42)
+# train, test = train_test_split(X, test_size=0.3, random_state=42)
 
 X_train = train.drop(["label"], axis=1)
 y_train = train["label"]
@@ -94,55 +111,69 @@ y_test = test["label"]
 train_pool = Pool(X_train, y_train, text_features=text_features, feature_names=list(X_train))
 test_pool = Pool(X_test, y_test, text_features=text_features, feature_names=list(X_train))
 
+bootstrap_type = ["Bayesian", "MVS", "Bernoulli"]
+depth = [8, 12]
+early_stopping_rounds = [10]
+grow_policy = ["SymmetricTree"]
+iterations = [500, 2000]
+l2_leaf_reg = [0.03, 0.1, 1]
+leaf_estimation_method = ["Newton"]
+learning_rate = [0.01, 0.03, 0.1]
+min_data_in_leaf = [1, 10]
+random_seed = [42]
+random_strength = [1, 8, 12]
+
 cb_params = {
-    "bootstrap_type": "Bernoulli",
-    "depth": 4,
-    "early_stopping_rounds": 10,
-    "grow_policy": "Lossguide",
-    "iterations": 5000,
-    "l2_leaf_reg": 0.5,
-    "leaf_estimation_method": "Newton",
-    "learning_rate": 0.01,
-    "max_leaves": 100,
-    "min_data_in_leaf": 10,
-    "random_seed": 42,
-    "random_strength": 5,
-    "verbose": False,
+    "bootstrap_type": bootstrap_type,
+    "depth": depth,
+    "early_stopping_rounds": early_stopping_rounds,
+    "grow_policy": grow_policy,
+    "iterations": iterations,
+    "l2_leaf_reg": l2_leaf_reg,
+    "leaf_estimation_method": leaf_estimation_method,
+    "learning_rate": learning_rate,
+    # "max_leaves": max_leaves,
+    "min_data_in_leaf": min_data_in_leaf,
+    "random_seed": random_seed,
+    "random_strength": random_strength,
 }
 
-model = CatBoostRegressor(**cb_params)
-model.fit(train_pool, eval_set=test_pool, verbose=100)
+param_grid = ParameterGrid(cb_params)
+n_combinations = len(list(param_grid))
+print(f"Number of combinations: {n_combinations}")
 
-x = 1
+f1_prev = 0
+for i, param in enumerate(param_grid):
+    print()
+    print(f"Training model {i + 1} with following parameters:")
+    print(param)
+    model = CatBoostRegressor(**param)
+    model.fit(train_pool, eval_set=test_pool, verbose=False)
+    print(f"Evaluating model {i + 1}...")
+    precision, recall, f1 = evaluate_model(model, cols)
+    if f1 > f1_prev:
+        f1_prev = f1
+        print(f"Precision: {precision}")
+        print(f"Recall: {recall}")
+        print(f"F1: {f1}")
+        if f1_prev > 0.64:
+            pickle_save(
+                {
+                    "model": model,
+                    "prediction": precision,
+                    "recall": recall,
+                    "f1": f1,
+                }
+            )
 
-
-#         X = data_part.drop(["rating_10"], axis=1)
-#         y = data_part["rating_10"]
-#         return X, y
-
-#     X_learn, y_learn = preprocess_data_part(learn)
-#     X_test, y_test = preprocess_data_part(test)
-
-#     return X_learn, X_test, y_learn, y_test
-
-
-# X_train, X_test, y_train, y_test = get_processed_rotten_tomatoes()
-
-
-# self.id,
-# self.num_statements,
-# self.instance_overlap,
-# self.subclass_overlap,
-# self.description_overlap,
-# self.instance_names,
 
 # model = ensemble_catboost_regression(features, labels)
 # pickle_save(model, f"{PICKLE_FILE_NAME}-l3-model")
 # model = pickle_load("validation-2022-bing-l3-model", is_dump=True)
 
 # ----- Evaluate model -----
-precision, recall, f1 = evaluate_model(model, cols)
+# precision, recall, f1 = evaluate_model(model, cols)
 
-print(f"Precision: {precision}")
-print(f"Recall: {recall}")
-print(f"F1: {f1}")
+# print(f"Precision: {precision}")
+# print(f"Recall: {recall}")
+# print(f"F1: {f1}")
