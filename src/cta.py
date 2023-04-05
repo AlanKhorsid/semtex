4cfc09f63f55c39ead1f0fd7977e2895e955435b
Adam Stück <adam@adast.xyz>
2023-04-05 13:30:32 +0200

    CTA: inital dumb method

--- src/cta.py
+++ src/cta.py
@@ -1,3 +1,10 @@
+from util import (
+        progress,
+        pickle_save,
+        pickle_load,
+        )
+from classes import (Column)
+
 #----CTA PLAN----#
 # input: list of candidates from CEA
 # check if the candidates have the same instance of
@@ -8,3 +15,16 @@
 #   if limit is hit, the largest overlap becomes the type (in case of errors in CEA)
 #
 # output: common type annotation
+
+def cta_no_query(candidates):
+    typedict = dict()
+
+    for cand in candidates:
+        for instance in cand.instances:
+            if instance in typedict:
+                typedict[instance] = typedict[instance] + 1
+            else:
+                typedict[instance] = 1
+
+PICKLE_FILE_NAME = "test-2022-bing"
+cols: list[Column] = pickle_load(f"{PICKLE_FILE_NAME}", is_dump=True)
