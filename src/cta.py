fc2a1d0fbe43a4b8e755797a3fb5defd7b12d190
asa19aau <asa19@student.aau.dk>
2023-04-19 11:31:32 +0200

    added some debug code to cta_no_query and started working on querying for instance values

--- src/cta.py
+++ src/cta.py
@@ -2,7 +2,7 @@ from util2 import (
         pickle_load,
         )
 from classes2 import Candidate
-from _requests import wikidata_entity_search
+from _requests import wikidata_fetch_entities, get_entity
 
 #----CTA PLAN----#
 # input: list of candidates from CEA
@@ -15,11 +15,13 @@ from _requests import wikidata_entity_search
 #
 # output: common type annotation
 
-def cta_no_query(candidates):
+def cta_no_query(candidates, correct=None, print_output=False):
     typedict = dict()
     best_value = 0
     best_key = 0
+    correct_key = None
     amount_of_nonetypes = 0
+    confidence = 0
 
     for candidate in candidates:
         if candidate == None:
@@ -37,22 +39,39 @@ def cta_no_query(candidates):
         if value > best_value:
             best_key = key
             best_value = value
-    print(f"return key: {best_key} confidence: {best_value/len(candidates)} percentage Nonetypes: {amount_of_nonetypes/len(candidates)}")
-    if amount_of_nonetypes == len(candidates):
-        print("=====ALL NONTYPES=====")
-        return None
+        if correct != None and key == correct:
+            correct_key = key
+
+    confidence = best_value/(len(candidates)-amount_of_nonetypes)
+
+    while confidence != 1:
+        fetch_ids = []
+        #query wikidata on subclass of and part of on each key in typedict
+        for key, value in typedict.items():
+            #add key to fetch list
+            fetch_ids.append(key)
+
+        break
+
+    if print_output:
+        print(f"return key: {best_key} confidence: {confidence} best-value/len(candidates): {best_value/len(candidates)} percentage Nonetypes: {amount_of_nonetypes/len(candidates)}")
+        if amount_of_nonetypes == len(candidates):
+            print("=====ALL NONTYPES=====")
+            return None
+        if correct_key != best_key and correct_key != None:
+            print(f"Found the correct key in the dataset, it is not the return key: {correct_key}")
+            print(f"return key: {best_key} confidence: {confidence} best-value/len(candidates): {best_value/len(candidates)} percentage Nonetypes: {amount_of_nonetypes/len(candidates)}")
 
     return best_key
 
 if __name__ == "__main__":
-    PICKLE_FILE_NAME = "cea_results"
+    PICKLE_FILE_NAME = "cea_results_test"
     candidates: list[Candidate] = pickle_load(f"{PICKLE_FILE_NAME}", is_dump=False)
     successes = 0
     failures = 0
     for cand in candidates:
-        result = cta_no_query(cand['candidates'])
+        result = cta_no_query(cand['candidates'], correct=cand['cta'], print_output=True)
         if result == cand['cta']:
-            print(f"correct result found: {cand['cta']}")
             successes += 1
         else:
             print("======DID NOT FIND INSTANCE======")
