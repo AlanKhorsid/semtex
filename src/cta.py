f48c7b09e48c0f2eb79f3a19d3f6793c16b83294
asa19aau <asa19@student.aau.dk>
2023-04-21 12:39:32 +0200

    restructed cta_retreiver (formerly cta_no_query) to take a list of lists of candidates to allow for more efficient wikidata querying. wikidata querying needs restructuring still

--- src/cta.py
+++ src/cta.py
@@ -17,46 +17,71 @@ from _requests import wikidata_fetch_entities, get_entity
 
 
 #TODO: rename cta_no_query to cta
-def cta_no_query(candidates, correct = None, print_output = False, max_depth = 0):
-    instance_dict = dict()
-    best_value = 0
-    best_key = 0
-    correct_key = None
-    amount_of_nonetypes = 0
-    current_depth = 0
-    confidence = 0
+def cta_retriever(candidates_list, correct = None, print_output = False, max_depth = 0):
+    result_ids = []
+    instance_dicts = []
+    confidence_vector = []
+    correct_key_vector = []
+    index = 0
+    for i in range(len(candidates_list)):
+        result_ids.append(None)
+        instance_dicts.append(dict())
+        confidence_vector.append(0)
+        if correct == None:
+            correct_key_vector.append(None)
+        else:
+            correct_key_vector = correct
 
-    #get instance_dict and amount of 
-    instance_dict, amount_of_nonetypes = add_to_instance_dict(candidates, instance_dict)
 
-    best_key, best_value, correct_key = find_best_key(instance_dict, correct=correct) 
 
-    confidence = best_value/(len(candidates)-amount_of_nonetypes)
-    if confidence > 1:
-        raise ValueError()
+    for candidates in candidates_list:
+        best_value = 0
+        best_key = 0
+        correct_key = correct_key_vector[index]
+        amount_of_nonetypes = 0
+        current_depth = 0
+        confidence = 0
 
-    while confidence < 1 and current_depth < max_depth:
-        current_depth += 1
-        current_best_value = best_value
-        current_best_key = best_key
-        #query wikidata on subclass of and part of on each key in instance_dict
-        instance_dict = cta_query(instance_dict)
-        best_key, best_value, correct_key = find_best_key(instance_dict, correct=correct)
+        #get instance_dict and amount of 
+        instance_dicts[index], amount_of_nonetypes = add_to_instance_dict(candidates, instance_dicts[index])
+
+        best_key, best_value, correct_key = find_best_key(instance_dicts[index], correct_key=correct_key) 
+
         confidence = best_value/(len(candidates)-amount_of_nonetypes)
-        if confidence < current_best_value/(len(candidates)-amount_of_nonetypes):
-            best_value = current_best_value
-            best_key = current_best_key
-    
+        if confidence > 1:
+            print(f"index: {index}, best_value: {best_value}, dict: {instance_dicts[index]}, candidates length: {len(candidates)}")
+            for cand in candidates:
+                print(cand.id)
+            raise ValueError()
 
-    if print_output:
-        if amount_of_nonetypes == len(candidates):
-            print("=====ALL NONTYPES=====")
-            return None
-        if correct_key != best_key and correct_key != None:
-            print(f"Found the correct key in the dataset, it is not the return key: {correct_key}")
-            print(f"return key: {best_key} confidence: {confidence} best-value/len(candidates): {best_value/len(candidates)} percentage Nonetypes: {amount_of_nonetypes/len(candidates)}")
+        while confidence < 1 and current_depth < max_depth:
+            current_depth += 1
+            current_best_value = best_value
+            current_best_key = best_key
+            #query wikidata on subclass of and part of on each key in instance_dict
+            instance_dicts[index] = cta_query(instance_dicts[index])
+            best_key, best_value, correct_key = find_best_key(instance_dicts[index], correct=correct)
+            confidence = best_value/(len(candidates)-amount_of_nonetypes)
+            if confidence < current_best_value/(len(candidates)-amount_of_nonetypes):
+                best_value = current_best_value
+                best_key = current_best_key
+        
 
-    return best_key
+        if print_output:
+            if amount_of_nonetypes == len(candidates):
+                print("=====ALL NONTYPES=====")
+                best_key = 0
+            if correct_key != best_key and correct_key != None:
+                print(f"Found the correct key in the dataset, it is not the return key: {correct_key}")
+                print(f"return key: {best_key} confidence: {confidence} best-value/len(candidates): {best_value/len(candidates)} percentage Nonetypes: {amount_of_nonetypes/len(candidates)}")
+        result_ids[index] = best_key
+        if result_ids[index] == 0:
+            print("HAD ZERO VALUE")
+
+        index += 1
+
+    return result_ids
+
 
 def cta_query(instance_dict : dict):
     fetch_ids = []
@@ -78,57 +103,80 @@ def cta_query(instance_dict : dict):
 def add_to_instance_dict(candidates, instance_dict, tree_exploration = False):
     amount_of_nonetypes = 0
     for candidate in candidates:
+        discovered_ids = []
         if candidate == None:
             amount_of_nonetypes += 1
             continue
+
         for statement in candidate.statements:
             if ((statement.property == 31 and tree_exploration == False) or (statement.property == 279 and tree_exploration == True)):
                 #print(f"statement value: {statement.value}")
+                if statement.value in discovered_ids:
+                    #print(f"continue called statement_value: {statement.value}")
+                    continue
+
+
                 if statement.value in instance_dict:
                     instance_dict[statement.value] = instance_dict[statement.value] + 1
+
                 else:
                     instance_dict[statement.value] = 1
+                
+                discovered_ids.append(statement.value)
+
+        discovered_ids = []
+
     return instance_dict, amount_of_nonetypes
 
-def find_best_key(instance_dict, correct = None):
+def find_best_key(instance_dict, correct_key = None):
     best_key = None
     best_value = 0
-    correct_key = None
+    result_key = None
     for key, value in instance_dict.items():
         if value > best_value:
             best_key = key
             best_value = value
-        if correct != None and key == correct:
-            correct_key = key
+        if correct_key != None and key == correct_key:
+            result_key = key
 
-    return best_key, best_value, correct_key
+    return best_key, best_value, result_key
 
 
 if __name__ == "__main__":
     #cta_query({618123: 1})
 
     PICKLE_FILE_NAME = "cea_results_test"
-    candidates: list[Candidate] = pickle_load(f"{PICKLE_FILE_NAME}", is_dump=False)
-    candidates = candidates[0:200]
+    candidates_objects: list[Candidate] = pickle_load(f"{PICKLE_FILE_NAME}", is_dump=False)
+    candidates_objects = candidates_objects[0:100]
+    candidates = []
     successes = 0
     failures = 0
     max_depth_failures = 0
     max_depth_successes = 0
-    for cand in candidates:
-        result1 = cta_no_query(cand['candidates'], correct=cand['cta'], max_depth = 0)
-        result2 = cta_no_query(cand['candidates'], correct=cand['cta'], max_depth = 1)
-        if result1 == cand['cta']:
+    index = 0
+
+    for cand in candidates_objects:
+        candidates.append(cand['candidates'])
+
+    result1 = cta_retriever(candidates)
+    result2 = cta_retriever(candidates)
+
+    print(result1)
+
+    for cand in candidates_objects:
+        if result1[index] == cand['cta']:
             successes += 1
         else:
             print("======DID NOT FIND INSTANCE======")
-            print(f"expected: {cand['cta']} result: {result1}")
+            print(f"expected: {cand['cta']} result: {result1[index]}")
             failures += 1
-        if result1 != result2 and result1 == cand['cta']:
+        if result1[index] != result2[index] and result1[index] == cand['cta']:
             max_depth_failures += 1
             print(f"max depth = 1 failed when max depth = 0 didn't")
-            print(f"expected: {cand['cta']} result: {result2}")
-        elif result1 != result2 and result2 == cand['cta']:
+            print(f"expected: {cand['cta']} result: {result2[index]}")
+        elif result1[index] != result2[index] and result2[index] == cand['cta']:
             max_depth_successes += 1
+        index += 1
     print(f"successes: {successes}   failures: {failures}")
     print(f"success percentage: {successes/(successes+failures)}")
     print(f"max depth successes: {max_depth_successes}   max depth failures: {max_depth_failures}")
